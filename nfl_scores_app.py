1cd8a440ae4738dd71d38214a6293d430e575479
Peter <lilv447@gmail.com>
2025-09-08 22:33:46 -0400

    completed games extraction method added

--- nfl_scores_app.py
+++ nfl_scores_app.py
@@ -7,8 +7,8 @@ BASE_URL = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboar
 def main():
     api_response = extract.make_api_request(BASE_URL)
     games = extract.extract_api_data(api_response)
-    games_status = parse.extract_status(games)
-    print(games_status)
+    completed_games = parse.extract_completed_games(games)
+    print(completed_games)
 
 
 if __name__ == "__main__":
--- parse.py
+++ parse.py
@@ -3,12 +3,13 @@ from typing import List
 from extract import GameRow
 
 
-# TEST: Still being tested, currently returning all statuses not only
-# completed games
-# TODO: check for Final status and only return those.
-def extract_status(games: List[GameRow]) -> List[str]:
+def extract_completed_games(games: List[GameRow]) -> List[str]:
     """Returns list of games that have status of completed"""
     completed_games = []
     for game in games:
-        completed_games.append(game.get("status", {}))
+        if game["status"] == "Final":
+            completed_games.append(game)
     return completed_games
+
+
+# TODO: write a method to pull the winners from the final games
