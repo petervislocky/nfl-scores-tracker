69a4d2945c3b9bd7fbfdab79375dc3348e997a73
Peter <lilv447@gmail.com>
2025-11-02 00:50:04 -0400

    Started converting to using number indecies to access columns and rows

--- spreadsheet_parse.py
+++ spreadsheet_parse.py
@@ -4,11 +4,19 @@ from openpyxl import load_workbook
 
 
 class ParseSpreadsheet:
-
-    def __init__(self, spreadsheet_path: str=r"./spreadsheet/NFL_Poop_Picks_2025-26.xlsx", sheet: str="Week 1"):
+    def __init__(
+        self,
+        spreadsheet_path: str = r"./spreadsheet/NFL_Poop_Picks_2025-26.xlsx",
+        sheet: str = "Week 1",
+    ):
         self.path = spreadsheet_path
         self.workbook = load_workbook(spreadsheet_path)
         self.sheet = self.workbook[sheet]
+
+        self.PICK_COLUMNS = [4, 5, 6, 7]  # E, F, G, H
+        self.RESULT_COLUMNS = [9, 10, 11, 12]  # J, K, L, M
+
+        # TODO: Stop accessing values via letter reference, use num index instead
 
     def get_cell(self, cell_ref: str) -> str:
         """Get value stored in given cell.
@@ -20,6 +28,7 @@ class ParseSpreadsheet:
 
     def set_cell(self, cell_ref: str, value: Any) -> None:
         self.sheet[cell_ref] = value
+
 
 ps = ParseSpreadsheet()
 print(ps.get_cell("B2"))
